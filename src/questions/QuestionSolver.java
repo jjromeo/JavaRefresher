7306d7fa3dd23c3609f782452e66bf52e3a798cf
Jerome Pratt <jerome.pratt@outlook.com>
2015-01-21 16:27:00 +0000

    completed second exercise

--- src/questions/QuestionSolver.java
+++ src/questions/QuestionSolver.java
@@ -6,15 +6,45 @@ import java.util.Arrays;
 public class QuestionSolver {
 
 	public String[] selectElementsStartingWithA(String[] input) {
+		
 		ArrayList<String> aElements = new ArrayList<String> (); 
+		
 		for(int i = 0; i < input.length; i++) {
-			if (input[i].charAt(0) == 'A' || input[i].charAt(0) == 'a') {
+			
+			if (input[i].charAt(0) == 'a') {
+				
 				aElements.add(input[i]);
+				
 			}
+			
 		}
 		String[] output = new String[aElements.size()];
+		
 		output = aElements.toArray(output);
+		
 		return output;
+		
 	}
 
+	public String[] selectElementsStartingWithVowel(String[] input) {
+		
+		ArrayList<String> aElements = new ArrayList<String> (); 
+		
+		for(int i = 0; i < input.length; i++) {
+			char[] vowels = {'a', 'e', 'i', 'o', 'u'};
+			
+			if (new String(vowels).indexOf((input[i].charAt(0))) >= 0) {
+				System.out.println(input[i]);
+				aElements.add(input[i]);
+				
+			}
+			
+		}
+		String[] output = new String[aElements.size()];
+		
+		output = aElements.toArray(output);
+		
+		return output;
+		
+	}
 }
--- src/questions/questionsTest.java
+++ src/questions/questionsTest.java
@@ -9,13 +9,20 @@ import org.junit.Test;
 public class questionsTest {
 
 	@Test
-	public void test() {
+	public void selectElementsStartingWithA() {
 		QuestionSolver qSolver = new QuestionSolver();
 		String[] input = {"bananas", "apples", "pears", "avocados"};
 		String[] output = {"apples", "avocados"};
 		assertArrayEquals(output, qSolver.selectElementsStartingWithA(input));
 	}
 
+	@Test
+	public void selectElementsStartingWithVowel() {
+		QuestionSolver qSolver = new QuestionSolver();
+		String[] input = {"john", "david", "omar", "fred", "idris", "angela"};
+		String[] output = {"omar", "idris", "angela"};
+		assertArrayEquals(output, qSolver.selectElementsStartingWithVowel(input));
+	}	
 
 
 }
